7dd8583ce08ba78ffaebf623cd4a7c923efa00e4
vijayrmourya <vijayrmourya@gmail.com>
2024-09-17 16:35:29 +0000

    updated module to use variables

--- data.tf
+++ data.tf
@@ -34,6 +34,3 @@ data "http" "my_ip" {
   url = "https://checkip.amazonaws.com/"
 }
 
-locals {
-  my_ip = chomp(data.http.my_ip.response_body)
-}
--- resources.tf
+++ resources.tf
@@ -2,55 +2,20 @@ module "instane_with_key_iam" {
   source = "../terraform-modules/instance_with_key_pair"
 
   ami_id                              = data.aws_ami.ubuntu_ami.id
-  instance_type                       = "t3.medium"
-  associate_public_ip_address_setting = true
-  instance_tags = {
-    Name = "vmourya-dev-deskop"
-  }
+  instance_type                       = var.instance_type
+  associate_public_ip_address_setting = var.instance_public_ip_enable
+  instance_tags                       = var.tag_instance
 
-  security_group_name        = "dev-desktop-rules"
-  security_group_description = "ingress and egress rules for personal development server"
+  security_group_name        = var.security_group_name
+  security_group_description = var.sg_description
   vpc_id                     = aws_default_vpc.default.id
-  ingress_rules = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["${local.my_ip}/32"]
-      description = "Allow SSH from my IP"
-    },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["${local.my_ip}/32"]
-      description = "Allow HTTP from my IP"
-    },
-    {
+  ingress_rules              = local.security_group_ingress
+  egress_rules               = local.security_group_egress
+  sg_tags                    = var.tag_sg
 
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["${local.my_ip}/32"]
-      description = "Allow HTTPS from my IP"
-    }
-  ]
-  egress_rules = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1" # Allows all protocols and ports
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "Allow all outbound traffic"
-    }
-  ]
-  sg_tags = {
-    Name = "dev-desktop-sg"
-  }
-
-  tls_private_key_algorithm              = "RSA"
-  tls_private_key_rsa_bits               = 4096
-  aws_key_pair_key_name                  = "dev-desktop-key"
-  local_file_private_key_filename        = "${path.module}/dev-desktopkey.pem"
-  local_file_private_key_file_permission = "0600"
+  tls_private_key_algorithm              = var.tls_key_algo
+  tls_private_key_rsa_bits               = var.key_rsa_bits
+  aws_key_pair_key_name                  = var.key_name
+  local_file_private_key_filename        = local.key_out_path
+  local_file_private_key_file_permission = var.key_permissions
 }
